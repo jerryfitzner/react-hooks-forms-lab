070c64f65d18c2893e0b66f3e6cec6cad36239f4
Jerry Fitzner <jfitzner1@gmail.com>
2022-05-05 15:42:29 -0400

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,15 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  const handleFormSubmit = (e) => {
+    console.log(e);
+    setItems([...items, e]);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={ handleFormSubmit }/>
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [newItem, setNewItem] = useState({
+    id: '',
+    name: "",
+    category: "Produce"
+  });
+
+  const submitFunc = (e) => {
+    e.preventDefault();
+    console.log(newItem);
+    onItemFormSubmit(newItem);
+  }
+
+  const submitFun = (e) => {
+    setNewItem({
+      ...newItem,
+      id: uuid(),
+      [e.target.name]: e.target.value
+    })
+  }
+
+  
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={ submitFunc }>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={submitFun} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={submitFun}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
